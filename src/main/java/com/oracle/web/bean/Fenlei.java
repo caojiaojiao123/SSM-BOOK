ec3bb4882d6839f0b09b0f13fd529bd58550b7d6
caojiaojiao <2638003630@qq.com>
2019-05-21 14:54:29 +0800

    第三次提交

--- src/main/java/com/oracle/web/bean/Fenlei.java
+++ src/main/java/com/oracle/web/bean/Fenlei.java
@@ -2,44 +2,42 @@ package com.oracle.web.bean;
 
 import java.util.List;
 
-import com.oracle.web.controller.Autowired;
-import com.oracle.web.controller.Controller;
-import com.oracle.web.controller.HttpServletRequest;
-import com.oracle.web.controller.RequestMapping;
-import com.oracle.web.controller.Scope;
-
-@Controller
-@Scope(value = "prototype")
-
-
 public class Fenlei {
 	
+	private int id;
 	
-	@Autowired
+	private String name;
+
+	public Fenlei(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public Fenlei() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Fenlei [id=" + id + ", name=" + name + "]";
+	}
 	
-    private Integer id;
-     @Autowired
-    private String name;
-     
-    @RequestMapping(value = "/addUI", method = RequestMethod.GET)
-    public Integer getId(HttpServletRequest request) {
-    	
-    	List<School> list = name.list();
-    	
-    	request.setAttribute("sList", list);
-    	
-        return "add";
-    }
-    @RequestMapping(value = "/monster/{monsterId}", method = RequestMethod.DELETE)
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name == null ? null : name.trim();
-    }
 }
